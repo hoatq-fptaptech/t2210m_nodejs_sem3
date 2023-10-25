2dcfb1945e5754d69996c00c1c20d484b7c168b2
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-10-25 11:24:36 +0700

    validate

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,15 @@
 const userModel = require("./../models/user.model");
 const bcrypt = require("bcryptjs");
+const {validationResult} = require("express-validator");
 exports.register = function(req,res){
     res.render("auth/register");
 };
 exports.postRegister = async function(req,res){
     const data = req.body;    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.send(errors.array());
+    }
     try {
         const salt = await bcrypt.genSalt(10);
         const hashed = await bcrypt.hash(data.password,salt);
@@ -19,6 +24,24 @@ exports.postRegister = async function(req,res){
 exports.login =  function(req,res){
    res.render("auth/login");     
 }
-exports.postLogin = function(req,res){
-    
+exports.postLogin = async function(req,res){
+    const email = req.body.email;
+    const pwd = req.body.password;
+    try {
+        // b1-  dùng email tìm user trong db -> nếu ko có báo lỗi email hoặc password ko đúng
+        const u = await userModel.findOne({email:email});
+        if(u == null){
+            return res.send("Email or Password is not correct");
+        }
+        // b2 -  so sanh password - dùng cơ chế hash verify để so sánh
+        const verify = bcrypt.compare(password,u.password);// return true/false
+        if(!verify){
+            return res.send("Email or Password is not correct");
+        }
+        // b3- phản hồi khi đúng
+        return res.send("Log in successfully");
+
+    } catch (error) {
+        return res.send(error);
+    }
 }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,28 @@
 const mongoose = require("mongoose");
 const user_schema = new mongoose.Schema({
     // _id
-    full_name: String,
-    email: String,
-    password:String
+    full_name: {
+        type: String,
+        required:[true,"Trường này bắt buộc phải nhập dữ liệu"],
+        minLength:[6,"Độ dài tối thiểu 6"]
+    },
+    email: {
+        type:String,
+        required:true,
+        minLength:6,
+        unique:true,
+        validate: {
+            validator: (v)=>{
+                // bộ quy tắc kiểm tra email
+                const rule = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;// email regex
+                return v.match(rule);
+            },
+            message: (t)=> "Giá trị vừa nhập không phải email"
+        }
+    },
+    password:{
+        type:String,
+        required:true
+    }
 });
 module.exports = mongoose.model("User",user_schema);// users
